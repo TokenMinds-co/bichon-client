afd721a694241116f84e69c235523d22205f69e8
Danzrrr <graita.sukma@gmail.com>
2024-10-01 16:05:54 +0700

    init: ticket support page

---
+++ app/support/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TicketSupportPage = () => {
+  return (
+    <main className="flex w-screen h-screen items-center justify-center bg-gray-800 p-5">
+      <h1 className="text-white text-2xl">Ticket Support Page</h1>
+    </main>
+  );
+};
+
+export default TicketSupportPage;
