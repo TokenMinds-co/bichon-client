203971f05a0f88b4794303f005515632363033cc
Kevin Tanuwijaya <kevintanuwijayasao@gmail.com>
2024-10-04 14:05:41 +0700

    fix: vercel deploy error

--- app/payment/result/page.tsx
+++ app/payment/result/page.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { Suspense } from "react";
 
 const PaymentResult = () => {
   const searchParams = useSearchParams();
@@ -13,25 +13,29 @@ const PaymentResult = () => {
   const canceled = searchParams.get("canceled");
 
   return (
-    <div className="flex flex-col gap-4 text-lg text-white font-bold items-center text-center justify-center h-screen w-screen">
-      {success && <p>Order placed! You will receive an email confirmation.</p>}
-      {canceled && (
-        <p>
-          Order canceled <br /> Continue to shop around and checkout when you’re
-          ready.
-        </p>
-      )}
-      {!success && !canceled && (
-        <p>
-          Awaiting payment status. Check your dashboard to track your
-          transactions.
-        </p>
-      )}
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="flex flex-col gap-4 text-lg text-white font-bold items-center text-center justify-center h-screen w-screen">
+        {success && (
+          <p>Order placed! You will receive an email confirmation.</p>
+        )}
+        {canceled && (
+          <p>
+            Order canceled <br /> Continue to shop around and checkout when
+            you’re ready.
+          </p>
+        )}
+        {!success && !canceled && (
+          <p>
+            Awaiting payment status. Check your dashboard to track your
+            transactions.
+          </p>
+        )}
 
-      <Link href={"/payment"}>
-        <Button>Buy Again</Button>
-      </Link>
-    </div>
+        <Link href={"/payment"}>
+          <Button>Buy Again</Button>
+        </Link>
+      </div>
+    </Suspense>
   );
 };
 
