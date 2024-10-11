7a0d6e22e66af7ea62de3e650d0cefe9b389391e
Danzrrr <graita.sukma@gmail.com>
2024-10-11 14:55:33 +0700

    feat: kyc status and redirection

--- components/kyc/FormKYC.tsx
+++ components/kyc/FormKYC.tsx
@@ -1,29 +1,15 @@
 "use client";
 
 import { useAccount } from "@particle-network/connectkit";
-import { useRouter } from "next/navigation";
-import React, { useState } from "react";
-import SkewButton from "../shared/SkewButton";
-import Image from "next/image";
 import { generateAxiosInstance } from "@/lib/axios-client";
 import { useQuery } from "@tanstack/react-query";
-import StatusKYC from "./StatusKYC";
-
-interface UserData {
-  email: string;
-  address: string;
-}
+import Loader from "../shared/Loader";
+import RenderKYC from "./RenderKYC";
 
 const FormKYC = () => {
-  const router = useRouter();
   const { address, isConnected } = useAccount();
-  const [isSubmitting, setSubmitting] = useState(false);
-  const [userData, setUser] = useState<UserData>({
-    email: "",
-    address: address || "",
-  });
 
-  const { data: users } = useQuery({
+  const { data: users, isLoading } = useQuery({
     queryKey: ["get-types", address],
     queryFn: async () => {
       if (!address) return [];
@@ -37,77 +23,20 @@ const FormKYC = () => {
     enabled: !!address,
   });
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    setSubmitting(true);
-    e.preventDefault();
-    const currentAddress = address;
-    // push to /verify and set the email and address in the query params
-    router.push(
-      `/verify?email=${encodeURIComponent(
-        userData.email
-      )}&address=${currentAddress}`
-    );
-  };
-
   return (
-    <div className="flex flex-col w-full items-center justify-center">
-      {isConnected ? (
-        users && users[0]?.kyc !== null ? (
-          <StatusKYC status={users[0]?.kyc?.status} />
-        ) : (
-          <>
-            <div className="text-white text-center text-xl mb-10">
-              Before participating in ICO, you should complete KYC
-            </div>
-
-            <form
-              id="sumsub_form"
-              onSubmit={handleSubmit}
-              className="flex flex-col w-full h-full rounded-md max-w-md items-center justify-center gap-4 text-black bg-gray-700/30 p-10 z-20"
-            >
-              <input
-                type="email"
-                placeholder="Email"
-                className="w-full py-2 px-4 bg-transparent text-white border-[1px] border-white/50 outline-none active:border-blue-500 focus:border-blue-500"
-                value={userData.email}
-                onChange={(e) =>
-                  setUser({ ...userData, email: e.target.value })
-                }
-                required
-              />
-
-              <input
-                type="text"
-                placeholder="Address"
-                readOnly
-                className="w-full py-2 px-4 bg-transparent text-white border-[1px] border-white/50 outline-none"
-                value={address}
-              />
-
-              <SkewButton
-                className="px-8 mt-6 w-full"
-                disabled={isSubmitting}
-                type="submit"
-                variant="primary"
-                customClasses="skew-buy-widgets"
-              >
-                {isSubmitting ? "Submitting..." : "Start KYC"}
-              </SkewButton>
-            </form>
-
-            <Image // FLOATING DOG
-              className="absolute bottom-0 right-[20%] z-[10] w-[400px] aspect-square animate-fly"
-              alt="floating-dog"
-              width={50}
-              height={50}
-              src="/assets/floating/dog.svg"
-            />
-          </>
-        )
-      ) : (
+    <div className="flex flex-col w-full items-center justify-center text-white">
+      {!isConnected ? (
         <h1 className="text-2xl text-white text-center h-full">
           Connect Wallet To Continue
         </h1>
+      ) : (
+        <>
+          {isLoading || !address ? (
+            <Loader size="50" />
+          ) : (
+            <RenderKYC address={address} users={users ? users : []} />
+          )}
+        </>
       )}
     </div>
   );
--- components/kyc/StatusKYC.tsx
+++ components/kyc/StatusKYC.tsx
@@ -1,42 +1,79 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
 import SkewButton from "../shared/SkewButton";
 import Link from "next/link";
 import { KYCStatus } from "@/types/Response";
+import { useRouter } from "next/navigation";
 
-export default function StatusKYC({ status }: { status: KYCStatus }) {
-  const src =
-    status === "APPROVED"
-      ? "/assets/kyc/approved.png"
-      : status === "REJECTED"
-      ? "/assets/kyc/rejected.png"
-      : "/assets/kyc/on-progress.png";
+const statuses = [
+  {
+    list: ["REJECTED", "REQUIRES_ACTION", "RESUBMISSION_REQUESTED"],
+    image: "/assets/kyc/rejected.png",
+    code: 0,
+  },
+  {
+    list: ["APPROVED"],
+    image: "/assets/kyc/approved.png",
+    code: 1,
+  },
+  {
+    list: ["PROCESSING", "DOCUMENTS_REQUESTED", "PENDING"],
+    image: "/assets/kyc/on-progress.png",
+    code: 2,
+  },
+];
 
-  const label =
-    status === "APPROVED"
-      ? "approved"
-      : status === "REJECTED"
-      ? "not approved"
-      : "still pending";
+interface StatusKYCProps {
+  status: KYCStatus;
+  email: string;
+  address: string;
+}
+
+export default function StatusKYC({ status, email, address }: StatusKYCProps) {
+  const router = useRouter();
+  const imageSrc = statuses.find((s) => s.list.includes(status))
+    ?.image as string;
+  const statusCode = statuses.find((s) => s.list.includes(status))
+    ?.code as number;
+
+  const handleUpdateDocs = async () => {
+    router.push(
+      `/verify?email=${encodeURIComponent(email)}&address=${address}`
+    );
+  };
 
   return (
     <div className="font-spaceMono text-white space-y-8">
       <div className="text-2xl text-center">Your Application Status: </div>
 
       <div className="bg-gray-700/20 skew-widgets p-8 w-full sm:w-[500px] rounded-md flex flex-col gap-10 justify-center items-center">
-        <Image alt="kyc-status" width={150} height={150} src={src} />
+        <Image alt="kyc-status" width={150} height={150} src={imageSrc} />
 
-        <div className="text-lg">KYC is {label}</div>
-
-        <Link className="w-full" href={status === "REJECTED" ? "/kyc" : "/"}>
-          <SkewButton
-            type="button"
-            customClasses="skew-buy-widgets"
-            className="w-full"
-          >
-            {status === "REJECTED" ? "Resubmit KYC" : "Back to home"}
-          </SkewButton>
-        </Link>
+        <div className="text-lg">Your KYC status is {status}</div>
+        <div className="flex">
+          {statusCode !== 0 ? (
+            <Link className="w-full" href="/">
+              <SkewButton
+                type="button"
+                customClasses="skew-buy-widgets"
+                className="w-full"
+              >
+                {status === "REJECTED" ? "Resubmit KYC" : "Back to home"}
+              </SkewButton>
+            </Link>
+          ) : (
+            <SkewButton
+              type="button"
+              customClasses="skew-buy-widgets"
+              className="w-full"
+              onClick={handleUpdateDocs}
+            >
+              Update your documents
+            </SkewButton>
+          )}
+        </div>
       </div>
     </div>
   );
