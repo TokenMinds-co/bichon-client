a407f09e955f05a005edd5dc0f03a2d6d0200f49
Kevin Tanuwijaya <kevintanuwijayasao@gmail.com>
2024-11-06 16:34:35 +0700

    fix: broken layout on card stacks

--- views/card-stack.tsx
+++ views/card-stack.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import React, { useEffect, useRef } from "react";
 
 const CardStack = () => {
@@ -36,7 +37,7 @@ const CardStack = () => {
     {
       title: "Bichon Defender AI wallet",
       content: (
-        <ul className="list-disc ml-4">
+        <ul className="list-disc ml-4 text-sm sm:text-base">
           <li>Ultimate security-focused Solana wallet</li>
           <li>Storage Vault for Long-Term Asset Protection</li>
           <li>Integrated Token Swap Feature</li>
@@ -48,22 +49,24 @@ const CardStack = () => {
         </ul>
       ),
       tailwindBg: "bg-gray-400",
+      pt: "pt-[11.5em]",
     },
     {
       title: " Bichon Defenders Next-Level Security Innovation",
       content: (
-        <ul className="list-disc ml-4">
+        <ul className="list-disc ml-4 text-sm sm:text-base">
           <li>Ngrave Zero for Key Management (Air-Gapped Cold Storage)</li>
           <li>Squads: Multisig on Solana</li>
           <li>Ngrave Graphene Backup</li>
         </ul>
       ),
       tailwindBg: "bg-gray-500",
+      pt: "pt-[12.5em]",
     },
     {
       title: "Bichon Defender AI Trading Bot",
       content: (
-        <ul className="list-disc ml-4">
+        <ul className="list-disc ml-4 text-sm sm:text-base">
           <li>Real-Time Analytics for Market Trends</li>
           <li>Continuous Market Adaptation</li>
           <li>Real-Time Analytics and Reporting</li>
@@ -71,11 +74,12 @@ const CardStack = () => {
         </ul>
       ),
       tailwindBg: "bg-gray-600",
+      pt: "pt-[13.5em]",
     },
     {
       title: "Governance Structure of the Defenders DAO",
       content: (
-        <ul className="list-disc ml-4">
+        <ul className="list-disc ml-4 text-sm sm:text-base">
           <li>Establish the Defenders DAO Governance Structure</li>
           <li>Proposal and Voting Mechanism</li>
           <li>DAO Membership and Compensation</li>
@@ -86,11 +90,12 @@ const CardStack = () => {
         </ul>
       ),
       tailwindBg: "bg-gray-700",
+      pt: "pt-[14.5em]",
     },
     {
       title: "Bichon Defender Exclusive NFTs",
       content: (
-        <ul className="list-disc ml-4">
+        <ul className="list-disc ml-4 text-sm sm:text-base">
           <li>Direct Access Pass - Basic Level (Total 5,000 NFTs)</li>
           <li>Elite Competitor NFT (Total 2,500 NFTs)</li>
           <li>Grandmaster NFT (Total 10 NFTs)</li>
@@ -98,11 +103,12 @@ const CardStack = () => {
         </ul>
       ),
       tailwindBg: "bg-gray-800",
+      pt: "pt-[15.5em]",
     },
     {
       title: "Metaverse Land Acquisition and City Creation",
       content: (
-        <ul className="list-disc ml-4">
+        <ul className="list-disc ml-4 text-sm sm:text-base">
           <li>Initial Land Allocation & Zoning</li>
           <li>Zoning Regulations</li>
           <li>Committee Formation and Governance</li>
@@ -112,35 +118,51 @@ const CardStack = () => {
         </ul>
       ),
       tailwindBg: "bg-gray-900",
+      pt: "pt-[16.5em]",
     },
   ];
 
+  let padding:
+    | "pt-[11.5em]"
+    | "pt-[12.5em]"
+    | "pt-[13.5em]"
+    | "pt-[14.5em]"
+    | "pt-[15.5em]"
+    | "pt-[16.5em]";
+
   return (
-    <div className="flex h-[2600px] overflow-hidden container mx-auto">
+    <div className="flex h-[2600px] lg:h-[2800px] overflow-hidden container mx-auto">
       <div className="h-full w-full">
         <div className="w-full container flex flex-col absolute px-10 items-start justify-start my-10 z-20">
           <ul
             className="list-none p-0 grid gap-32 grid-cols-1 pb-[calc(6*1.5em)] mb-[4vw] w-full"
             style={{ gridTemplateRows: "repeat(6, 30vh)" }}
           >
-            {list.map((item, index) => (
-              <li
-                key={index}
-                className={`sticky top-0 pt-[${11.5 + index * 1}em] h-full`}
-              >
-                <div
-                  ref={(el) => {
-                    observerRefs.current[index] = el;
-                  }}
-                  className={`${item.tailwindBg} flex-col gap-5 p-10 skew-widgets shadow-lg h-[400px] sm:h-[325px] flex transition-all duration-500`}
-                >
-                  <h2 className="text-lg xl:text-2xl text-left">
-                    {item.title}
-                  </h2>
-                  {item.content}
-                </div>
-              </li>
-            ))}
+            {list.map((item, index) => {
+              padding = item.pt as
+                | "pt-[11.5em]"
+                | "pt-[12.5em]"
+                | "pt-[13.5em]"
+                | "pt-[14.5em]"
+                | "pt-[15.5em]"
+                | "pt-[16.5em]";
+
+              return (
+                <li key={index} className={cn("sticky top-0 h-full", padding)}>
+                  <div
+                    ref={(el) => {
+                      observerRefs.current[index] = el;
+                    }}
+                    className={`${item.tailwindBg} flex-col gap-5 p-10 skew-widgets shadow-lg h-[400px] sm:h-[325px] flex transition-all duration-500`}
+                  >
+                    <h2 className="text-base sm:text-lg xl:text-2xl text-left">
+                      {item.title}
+                    </h2>
+                    {item.content}
+                  </div>
+                </li>
+              );
+            })}
           </ul>
         </div>
       </div>
