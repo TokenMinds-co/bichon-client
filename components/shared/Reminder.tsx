7a4dd36b4d09bcc6d12eab30a4d576fe1e12f99d
Kevin Tanuwijaya <kevintanuwijayasao@gmail.com>
2024-11-22 10:42:54 +0700

    feat: update reminder text

--- components/shared/Reminder.tsx
+++ components/shared/Reminder.tsx
@@ -18,18 +18,21 @@ const AddAccount = () => {
     <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
       <DialogContent className="sm:max-w-[425px] bg-[#000A19] text-white">
         <DialogHeader>
-         <div className="flex flex-row items-center gap-1"> <Image
-            width={40}
-            height={40}
-            src="/assets/bichon.png"
-            alt="bichon-logo"
-          />
-          <DialogTitle>Information:</DialogTitle></div>
+          <div className="flex flex-row items-center gap-1">
+            {" "}
+            <Image
+              width={40}
+              height={40}
+              src="/assets/bichon.png"
+              alt="bichon-logo"
+            />
+            <DialogTitle>Information:</DialogTitle>
+          </div>
         </DialogHeader>
 
         <div className="text-sm my-5">
-          To ensure seamless transaction on mobile devices, open the
-          https://bichondefender.com inside your wallets browser
+          To ensure seamless transaction on mobile devices, open{" "}
+          <strong>Bichon Defender</strong> inside your wallets browser
         </div>
 
         <DialogFooter>
